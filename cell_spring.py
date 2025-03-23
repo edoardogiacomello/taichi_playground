d95a36d0faaa7f940fd89394ee48b1dd8fc2157b
Edoardo Giacomello <edoardo.giacomello1990@gmail.com>
2025-03-23 22:05:25 +0100

    [Add] Simple static cell drawing

--- cell_spring.py
+++ cell_spring.py
@@ -1,19 +1,13 @@
 import taichi as ti
+import math
+
 ti.init(arch=ti.gpu)
 
-particle_struct = {
-    "pos": ti.math.vec2,
-    "vel": ti.math.vec2,
-    "acc": ti.math.vec2,
-    "mass": float,
-    "cell_id": int,
-    "is_nucleus": int,
-    "is_attached": int,
-}
+# TODO: Make also self.grid sparse
 
 @ti.data_oriented
 class Simulation():
-    def __init__(self, H=1024, W=1024, dt=1e-4, headless=False):
+    def __init__(self, H=512, W=512, dt=1e-4, headless=False):
         self.H = H
         self.W = W
         # Using old GUI to support Vulkan 1.4 on some machines
@@ -23,22 +17,31 @@ class Simulation():
         self.time = ti.field(dtype=float, shape=())
         self.dt = dt
 
+
+        # self.T_cell = ti.types.struct(cell_id=int, radius=int, particles_idx=int)
+        # self.S_cells = ti.root.dynamic(ti.i, 1, chunk_size=10)
+        # self.cells = self.T_cell.field()
+        # self.S_cells.place(self.cells)
+        self.cells = []
+
         # Create Nodes sparse structure
-        self.S_particles = ti.root.dynamic(ti.i, 10000, chunk_size=32)
-        self.particles = ti.Struct.field(particle_struct)
+        self.T_particle = ti.types.struct(pos=ti.math.vec2,
+                                          vel=ti.math.vec2,
+                                          acc=ti.math.vec2,
+                                          mass=float,
+                                          cell_id=int,
+                                          is_nucleus=int,
+                                          is_membrane=int,
+                                          is_attached=int)
+        self.S_particles = ti.root.dynamic(ti.i, 1, chunk_size=32)
+        self.particles = self.T_particle.field()
         self.S_particles.place(self.particles)
 
-
-        self.max_particles = ti.field(dtype=int, shape=(1,))
-        self.max_particles[0] = 10000
-        # Positions of each particle
-        self.pos = ti.Vector.field(2, dtype=float, shape=self.max_particles[0])
-        # Array to store the object id of each particle
-        self.part_to_obj = ti.field(dtype=int, shape=self.max_particles[0])
-        self.part_to_obj.fill(-1)
-        
         # UI Vars
-        self.selected_cell = -1
+        self.selected_cell = 0
+
+        # What is displayed on screen
+        self.render_grid = ti.field(dtype=float, shape=(H, W))
     
     @ti.kernel
     def update_particles(self):
@@ -47,28 +50,66 @@ class Simulation():
     def update(self):
         self.update_particles()
 
+    def spawn_cell(self, x: float = .5, y: float = .5, radius:float=.3):
+        cell_id = len(self.cells) + 1
+        self.add_cell(xc=x, yc=y, radius=radius, cell_id=cell_id)
+        self.cells.append(cell_id)
+        print(f"Added cell {cell_id}")
+    
     @ti.kernel
+    def add_cell(self, xc: float, yc: float, radius: float, cell_id: int):
+        
+        T = 16
+        R = 8
+        
+        # Add nucleus
+        nucleus = self.T_particle(pos=[xc, yc], 
+                                    mass=1, 
+                                    cell_id=cell_id, 
+                                    is_nucleus=1, 
+                                    is_membrane=0)
+        self.particles.append(nucleus)
+
+        for r in range(R):
+            inner_radius = radius * (1 - r / R)
+            for t in range(T):
+                theta = 2 * math.pi * t / T
+                x = xc + inner_radius * ti.cos(theta)
+                y = yc + inner_radius * ti.sin(theta)
+                is_membrane = 1 if r == 0 else 0  # Outer circle gets is_membrane = 1
+                new_particle = self.T_particle(pos=[x, y], 
+                                                mass=1, 
+                                                cell_id=cell_id, 
+                                                is_nucleus=0, 
+                                                is_membrane=is_membrane)
+                self.particles.append(new_particle)
+        
+
     def reset(self):
-        self.time[None] += self.dt
+        self.time[None] += self.dt        
+        self.spawn_cell(x=.7, y=.7, radius=.1)
+        self.spawn_cell(x=.3, y=.3, radius=.1)
         pass
+    
+    @ti.kernel
+    def render(self):
+        """
+            Draw (sparse) particles to the grid.
+        """
+
+        # Sparse particles cannot (apparently) be drawn using gui primitives since .to_numpy() would include also deactivated chunks
+        for p in range(self.particles.length()):
+            part = self.particles[p]
+            # TODO: Use quadratic interpolations?
+            self.render_grid[int(part.pos[0]*self.W), int(part.pos[1]*self.H)] = part.cell_id
 
 
     def draw(self):
         # Draw particles
-        to_draw = self.particles.pos.to_numpy()
-        self.gui.circles(to_draw, radius=1, color=0xFF0000)
+        self.render()
+        self.gui.set_image(self.render_grid)
         self.gui.show()
     
-    @ti.kernel
-    def add_particle(self, x: float, y: float, cell_id: int):
-
-        print("Adding Particle")
-        first_free_idx = self.particles.length()
-        self.particles[first_free_idx].is_nucleus = 1
-        self.particles[first_free_idx].pos[0] = x
-        self.particles[first_free_idx].pos[1] = y
-        print(self.particles[first_free_idx].pos)
-        print(self.particles.length())
 
     def parse_input(self):
         events = self.gui.get_events()
@@ -84,9 +125,9 @@ class Simulation():
             if self.gui.is_pressed(ti.ui.DOWN):
                 self.selected_cell = max(0, self.selected_cell - 1)
                 print(f"Selected cell: {self.selected_cell}")
-            if self.gui.is_pressed("a"):
-                x, y = self.gui.get_cursor_pos()                
-                self.add_particle(x, y, self.selected_cell)
+            # if self.gui.is_pressed("a"):
+            #     x, y = self.gui.get_cursor_pos()                
+            #     self.add_particle(x, y, self.selected_cell)
             
     def run(self):
         self.reset()
